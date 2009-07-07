afa0cbffb1c1d52720af1f345890ebb768ce2e26
Matthias Kestenholz <mk@spinlock.ch>
2009-07-07 22:43:26 +0200

    Add setup.sh script, update FeinCMS skeleton with register_templates call

--- co2monitor/models.py
+++ co2monitor/models.py
@@ -10,7 +10,17 @@ from feincms.content.richtext.models import RichTextContent
 from feincms.content.medialibrary.models import MediaFileContent
 from feincms.content.raw.models import RawContent
 
+Page.register_templates({
+    'title': 'Standard template',
+    'path': 'base.html',
+    'regions': (
+        ('main', _('Main content area')),
+        ('sidebar', _('Sidebar'), 'inherited'),
+        ('moodboard', _('Moodboard'), 'inherited'),
+        ),
+    })
+
 Page.register_extensions('navigation', 'seo', 'titles', 'translations')
-Page.create_content_type(RichTextContent)
+Page.create_content_type(RichTextContent, regions=('main',))
 MediaFileContent.default_create_content_type(Page)
 """
---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+mkdir lib
+cd lib
+git clone /var/cache/git/django/django.git
+git clone /var/cache/git/django/feincms.git
+git clone /var/cache/git/django/mptt.git
+cd ..
+ln -s lib/django/django django
+ln -s lib/feincms/feincms feincms
+ln -s lib/mptt/mptt mptt
+
+sudo setfacl -R -d -m u:www-data:rwx media
+sudo setfacl -R -m u:www-data:rwx media
+
+cat > secrets.py << "EOD"
+DATABASE_ENGINE = 'mysql'
+DATABASE_HOST = 'whale.internal'
+DATABASE_PORT = ''
+DATABASE_NAME = 'dbname'
+DATABASE_USER = 'dbname'
+DATABASE_PASSWORD = 'dbpasswd'
+SECRET_KEY = ''
+APP_MODULE = 'co2monitor'
+FORCE_DOMAIN = 'www.co2monitor.ch'
+
+"""
+CREATE DATABASE dbname DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE USER dbname@lemur.internal IDENTIFIED BY 'dbpasswd';
+GRANT ALL PRIVILEGES ON dbname.* TO dbname@lemur.internal;
+"""
+EOD
+
+echo "mysql -uroot -p -hwhale.internal"
