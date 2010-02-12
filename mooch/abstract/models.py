fd6e365c1bdd28bec425282e9284b7cf6718ff55
Stefan Reinhard <sr@feinheit.ch>
2010-02-12 12:35:19 +0100

    create initial module structure and import some common usefull models from metronom

---
+++ mooch/abstract/models.py
@@ -0,0 +1,80 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from mooch.accounts.middleware import get_current_user
+
+class CreateUpdateModel(models.Model):
+    """
+    Store timestamps for creation and last modification.
+    """
+
+    created = models.DateTimeField(_('Created'), auto_now_add=True)
+    modified = models.DateTimeField(_('Modified'), auto_now=True)
+
+    class Meta:
+        abstract = True
+        get_latest_by = 'created'
+        ordering = ('created',)
+
+class CreatorModel(CreateUpdateModel):
+    """
+    Store the currently logged-in user in addition to the timestamps above.
+    """
+
+    created_by = models.ForeignKey(User, related_name='created_%(class)s_set',
+        default=get_current_user, verbose_name=_('Created by'))
+
+    class Meta:
+        abstract = True
+
+class BaseModel(CreatorModel):
+    """
+    Often, we need a name and a notes field (which may be blank) for our
+    objects.
+    """
+
+    name = models.CharField(_('Name'), max_length=100)
+    notes = models.TextField(_('Notes'), blank=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('name',)
+
+    def __unicode__(self):
+        return self.name
+    
+class ContentTypeMixin(models.Model):
+    """
+    Simplify some of the needed boilerplate code for objects which can
+    have a foreign key to any other object.
+    """
+
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey()
+
+    class Meta:
+        abstract = True
+
+
+class ContentTypeManager(models.Manager):
+    def for_object_and_profile(self, obj, profile):
+        return self._for_object(obj, self.for_profile(profile))
+
+    def for_object(self, obj):
+        return self._for_object(obj, self.all())
+
+    def _for_object(self, obj, queryset):
+        """
+        Helper function which helps following reverse generic relations.
+        """
+
+        if isinstance(obj, models.Model):
+            return self.filter(
+                content_type=ContentType.objects.get_for_model(obj),
+                object_id=obj.pk)
+
+        # it's probably a model class, not an instance
+        return self.filter(
+            content_type=ContentType.objects.get_for_model(obj))
---
+++ mooch/accounts/middleware.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
+from django.utils.cache import patch_vary_headers
+from django.utils import translation
+try:
+    from threading import local
+except ImportError:
+    from django.utils._threading_local import local
+
+_thread_locals = local()
+
+def get_current_request():
+    return getattr(_thread_locals, 'request', None)
+
+def get_current_user():
+    return getattr(get_current_request(), 'user', None)
+
+class ThreadLocals(object):
+    """Middleware that stores the request object in thread local storage."""
+    def process_request(self, request):
+        _thread_locals.request = request
+
+
+class UserMiddleware(object):
+    def process_request(self, request):
+        if hasattr(request, 'user') and isinstance(request.user, User):
+            profile = request.user.get_profile()
+            if not profile.language:
+                profile.language = translation.get_language_from_request(request)
+                profile.save()
+            translation.activate(profile.language)
+
+            request.user.last_login = datetime.now()
+            request.user.save()
+        else:
+            language = translation.get_language_from_request(request)
+            translation.activate(language)
+
+        request.LANGUAGE_CODE = translation.get_language()
+
+    def process_response(self, request, response):
+        patch_vary_headers(response, ('Accept-Language',))
+        if 'Content-Language' not in response:
+            response['Content-Language'] = translation.get_language()
+        translation.deactivate()
+        return response
