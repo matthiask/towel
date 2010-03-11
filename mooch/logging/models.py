0c0000f4c0371d87cd81b221616253f05e7b3d08
Stefan Reinhard <sr@gmac.(none)>
2010-03-11 11:43:26 +0100

    extend logging stub

--- mooch/logging/models.py
+++ mooch/logging/models.py
@@ -4,9 +4,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
-from mooch.accounts.models import Profile
-from mooch.accounts.middleware import get_current_user
-from mooch.abstract.models import BaseModel, CreateUpdateModel
+from mooch.organisation.models import Project
+from mooch.abstract.models import CreateUpdateModel
 
 LOG_SOURCES = (
     ('WEB', 'Online'),
@@ -16,14 +15,15 @@ LOG_SOURCES = (
 )
 
 class LogEntry(CreateUpdateModel):
-    account = models.ForeignKey(Profile)
+    account = models.ForeignKey(User)
+    project = models.ForeignKey(Project, related_name="logentries", verbose_name=_('project'))
     title = models.CharField(_('title'), max_length=150)
     message = models.TextField(_('text'))
     source = models.CharField(_('origin'), choices=LOG_SOURCES, max_length=10)
     reported = models.DateTimeField(_('reported'), default=datetime.now)
     
     class Meta:
-        verbose_name = _('Log Entry')
-        verbose_name_plural = _('Log Entries')
+        verbose_name = _('log entry')
+        verbose_name_plural = _('log entries')
         ordering = ('-reported',)
 
