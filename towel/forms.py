5389ea48023e11c18b74c0b85a01eaed66a1b540
Matthias Kestenholz <mk@spinlock.ch>
2010-11-19 10:35:44 +0100

    Add ModelAutocompleteWidget

--- towel/forms.py
+++ towel/forms.py
@@ -2,6 +2,10 @@ import pickle
 
 from django import forms
 from django.db import models
+from django.forms.util import flatatt
+from django.utils import simplejson
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -137,3 +141,53 @@ def stripped_formfield_callback(field, **kwargs):
         kwargs['widget'] = forms.DateInput(attrs={'class': 'dateinput'})
 
     return field.formfield(**kwargs)
+
+
+class ModelAutocompleteWidget(forms.TextInput):
+    def __init__(self, attrs=None, url=None, queryset=None):
+        assert url or queryset, 'Provide either url or queryset'
+
+        self.url = url
+        self.queryset = queryset
+        super(ModelAutocompleteWidget, self).__init__(attrs)
+
+    def render(self, name, value, attrs=None, choices=()):
+        if value is None:
+            value = ''
+        final_attrs = self.build_attrs(attrs, type='hidden', name=name)
+        if value != '':
+            # Only add the 'value' attribute if a value is non-empty.
+            final_attrs['value'] = force_unicode(self._format_value(value))
+
+        hidden = u'<input%s />' % flatatt(final_attrs)
+
+        final_attrs['type'] = 'text'
+        final_attrs['name'] += '_ac'
+        final_attrs['id'] += '_ac'
+
+        try:
+            model = self.choices.queryset.get(pk=value)
+            final_attrs['value'] = force_unicode(model)
+        except (self.choices.queryset.model.DoesNotExist, ValueError, TypeError):
+            final_attrs['value'] = u''
+
+        ac = u'<input%s />' % flatatt(final_attrs)
+
+        js = '''<script type="text/javascript">
+$(function() {
+    $('#%(id)s_ac').autocomplete(%(source)s, {
+        matchContains: true, minChars: 2, max: 100
+    }).result(function(event, item) {
+        $('#%(id)s').val(item[1]).trigger('change');
+    });
+});
+</script>
+''' % {'id': attrs.get('id', name), 'name': name, 'source': self._source()}
+
+        return mark_safe(hidden + ac + js)
+
+    def _source(self):
+        if self.url:
+            return u'\'%s\'' % self.url
+        else:
+            return simplejson.dumps([(unicode(o), o.id) for o in self.queryset.all()])
