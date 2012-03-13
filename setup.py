fb829712ab7874bd1b598efbb4b3d6a1a31dfa61
Matthias Kestenholz <mk@spinlock.ch>
2012-03-13 08:28:39 +0100

    Convert towel to distutils

--- setup.py
+++ setup.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
 
+from distutils.core import setup
 import os
-from setuptools import setup, find_packages
+import setuplib
 
-import towel
+packages, package_data = setuplib.find_packages('towel')
 
 setup(name='towel',
-      version=towel.__version__,
-      description='Keeping you DRY since 2010',
-      long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read().decode('utf-8'),
-      author='Matthias Kestenholz',
-      author_email='mk@feinheit.ch',
-      url='http://github.com/matthiask/towel/',
-      license='BSD License',
-      platforms=['OS Independent'],
-      packages=find_packages(),
-      include_package_data=True,
-      )
+    version=__import__('towel').__version__,
+    description='Keeping you DRY since 2010',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read().decode('utf-8'),
+    author='Matthias Kestenholz',
+    author_email='mk@feinheit.ch',
+    url='http://github.com/matthiask/towel/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=packages,
+    package_data=package_data,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+)
