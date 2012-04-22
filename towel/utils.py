0c21ed173957cdd59da93527250652dc61d2b7d7
Matthias Kestenholz <mk@spinlock.ch>
2012-04-22 19:50:22 +0200

    Add a really simple template tag args/kwargs parser
    Really dumb, but better than depending on Django's internals.

--- towel/utils.py
+++ towel/utils.py
@@ -1,3 +1,6 @@
+import re
+
+from django import template
 from django.db.models.deletion import Collector
 
 
@@ -57,3 +60,55 @@ def safe_queryset_and(qs1, qs2):
             res = res.select_related()
 
     return res
+
+
+kwarg_re = re.compile("(?:(\w+)=)?(.+)")
+
+def parse_args_and_kwargs(parser, bits):
+    """
+    Parses template tag arguments and keyword arguments
+
+    Returns a tuple ``args, kwargs``.
+
+    Usage::
+
+        @register.tag
+        def custom(parser, token):
+            return CustomNode(*parse_args_and_kwargs(parser, token.split_contents()[1:]))
+
+        class CustomNode(template.Node):
+            def __init__(self, args, kwargs):
+                self.args = args
+                self.kwargs = kwargs
+
+            def render(self, context):
+                args, kwargs = resolve_args_and_kwargs(context, self.args, self.kwargs)
+                return self._render(context, *args, **kwargs):
+
+            def _render(self, context, ...):
+                # The real workhorse
+    """
+    args = []
+    kwargs = {}
+
+    for bit in bits:
+        match = kwarg_re.match(bit)
+        key, value = match.groups()
+        value = parser.compile_filter(value)
+        if key:
+            kwargs[key] = value
+        else:
+            args.append(value)
+
+    return args, kwargs
+
+
+def resolve_args_and_kwargs(context, args, kwargs):
+    """
+    Resolves arguments and keyword arguments parsed by ``parse_args_and_kwargs`` using
+    the passed context instance
+
+    See ``parse_args_and_kwargs`` for usage instructions.
+    """
+    return [v.resolve(context) for v in args], dict((k, v.resolve(context))
+        for k, v in kwargs.items())
