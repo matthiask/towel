2bd31ddf3aa8ea4c1afda336482b592ddff6cf05
Matthias Kestenholz <mk@spinlock.ch>
2012-05-24 11:27:25 +0200

    Change documentation files extension to .rst

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.txt'
+source_suffix = '.rst'
 
 # The encoding of source files.
 #source_encoding = 'utf-8'
