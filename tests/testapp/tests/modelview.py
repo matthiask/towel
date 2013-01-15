4f2dabc45f22a9ad6350ab33267e4bdf4a00b4ea
Matthias Kestenholz <mk@spinlock.ch>
2013-01-15 21:34:51 +0100

    Add tests for adding stuff through the ModelView

--- tests/testapp/tests/modelview.py
+++ tests/testapp/tests/modelview.py
@@ -20,3 +20,17 @@ class ModelViewTest(TestCase):
             'Family 6')
         self.assertEqual(self.client.get('/persons/0/').status_code, 404)
         self.assertEqual(self.client.get('/persons/a/').status_code, 404)
+
+    def test_crud(self):
+        self.assertContains(self.client.get('/persons/add/'), '<form', 1)
+        self.assertEqual(self.client.post('/persons/add/', {}).status_code,
+            200)
+        response = self.client.post('/persons/add/', {
+            'family_name': 'Blub',
+            'given_name': 'Blab',
+            'created': '2013-01-01 01:00:00',
+            })
+        person = Person.objects.get()
+        self.assertRedirects(response, person.get_absolute_url())
+        self.assertContains(self.client.get(person.get_absolute_url()),
+            'Blab Blub')
