f52b7dd3a7759864694390681819470bbc34e321
Matthias Kestenholz <mk@spinlock.ch>
2013-01-15 19:43:13 +0100

    Tests for safe_queryset_and

--- tests/testapp/tests/utils.py
+++ tests/testapp/tests/utils.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from towel.utils import related_classes
+from towel.utils import related_classes, safe_queryset_and
 
 from testapp.models import Person, EmailAddress
 
@@ -21,3 +21,35 @@ class UtilsTest(TestCase):
             set(related_classes(person)),
             set((Person, EmailAddress)),
             )
+
+    def test_safe_queryset_and(self):
+        def _transform_nothing(queryset):
+            pass
+
+        qs1 = EmailAddress.objects.search('blub').transform(
+            _transform_nothing).select_related()
+        qs2 = EmailAddress.objects.distinct().reverse().select_related(
+            'person')
+        qs3 = EmailAddress.objects.all()
+
+        qs = safe_queryset_and(safe_queryset_and(qs1, qs2), qs3)
+
+        self.assertEqual(qs._transform_fns, [_transform_nothing])
+        self.assertFalse(qs.query.standard_ordering)
+        self.assertEqual(qs.query.select_related, {'person': {}})
+        self.assertTrue(qs.query.distinct)
+
+        qs = safe_queryset_and(
+            EmailAddress.objects.select_related(),
+            EmailAddress.objects.select_related(),
+            )
+
+        self.assertTrue(qs.query.select_related)
+        self.assertFalse(qs.query.distinct)
+
+        qs = safe_queryset_and(
+            EmailAddress.objects.all(),
+            EmailAddress.objects.select_related(),
+            )
+
+        self.assertTrue(qs.query.select_related)
