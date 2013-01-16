85b9501a36c1ed2fe83fac658fe5e545636ca1c2
Matthias Kestenholz <mk@spinlock.ch>
2013-01-16 08:25:12 +0100

    Automatically open the coverage HTML report after running the testsuite with ./cov.sh

--- tests/cov.sh
+++ tests/cov.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-coverage run --branch --include="*towel*" ./manage.py test testapp
+coverage run --branch --include="*towel/towel*" ./manage.py test testapp
+coverage html
+if [ -x /usr/bin/xdg-open ] ; then /usr/bin/xdg-open htmlcov/index.html ; fi
