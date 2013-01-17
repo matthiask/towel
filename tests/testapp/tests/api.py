1110a3df4316e4745560bb19bf69677ac1fa3bf6
Matthias Kestenholz <mk@spinlock.ch>
2013-01-17 10:13:27 +0100

    Add an insufficient XML output test

--- tests/testapp/tests/api.py
+++ tests/testapp/tests/api.py
@@ -121,6 +121,13 @@ class APITest(TestCase):
             {u'error': u'No Person matches the given query.'},
             )
 
+        # XML
+        response = self.client.get('/api/v1/person/',
+            HTTP_ACCEPT='application/xml')
+        self.assertContains(response,
+            '<value type="string" name="family_name">',
+            20)
+
     def test_http_methods(self):
         response = self.client.options('/api/v1/')
         self.assertEqual(response.status_code, 200)
