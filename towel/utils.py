98d8f1927884ac4ad9e6b4e22cbe631f948aacba
Matthias Kestenholz <mk@spinlock.ch>
2013-02-14 15:54:38 +0100

    safe_queryset_and: Allow an arbitrary number of querysets

--- towel/utils.py
+++ towel/utils.py
@@ -24,9 +24,9 @@ def related_classes(instance):
     return collector.data.keys()
 
 
-def safe_queryset_and(qs1, qs2):
+def safe_queryset_and(head, *tail):
     """
-    Safe AND-ing of two querysets. If one of both queries has its
+    Safe AND-ing of querysets. If one of both queries has its
     DISTINCT flag set, sets distinct on both querysets. Also takes extra
     care to preserve the result of the following queryset methods:
 
@@ -36,41 +36,47 @@ def safe_queryset_and(qs1, qs2):
     * ``prefetch_related()``
     """
 
-    if qs1.query.distinct or qs2.query.distinct:
-        res = qs1.distinct() & qs2.distinct()
-    else:
-        res = qs1 & qs2
-
-    res._transform_fns = list(set(
-        getattr(qs1, '_transform_fns', [])
-        + getattr(qs2, '_transform_fns', [])))
-
-    if not (qs1.query.standard_ordering and qs2.query.standard_ordering):
-        res.query.standard_ordering = False
-
-    select_related = [qs1.query.select_related, qs2.query.select_related]
-    if False in select_related:
-        # We are not interested in the default value
-        select_related.remove(False)
-
-    if len(select_related) == 1:
-        res.query.select_related = select_related[0]
-    elif len(select_related) == 2:
-        if True in select_related:
-            # Prefer explicit select_related to generic select_related()
-            select_related.remove(True)
-
-        if len(select_related) > 0:
-            # If we have two explicit select_related calls, take any of them
-            res.query.select_related = select_related[0]
+    def _merge(qs1, qs2):
+        if qs1.query.distinct or qs2.query.distinct:
+            res = qs1.distinct() & qs2.distinct()
         else:
-            res = res.select_related()
+            res = qs1 & qs2
 
-    res._prefetch_related_lookups = list(
-        set(qs1._prefetch_related_lookups)
-        | set(qs2._prefetch_related_lookups))
+        res._transform_fns = list(set(
+            getattr(qs1, '_transform_fns', [])
+            + getattr(qs2, '_transform_fns', [])))
 
-    return res
+        if not (qs1.query.standard_ordering and qs2.query.standard_ordering):
+            res.query.standard_ordering = False
+
+        select_related = [qs1.query.select_related, qs2.query.select_related]
+        if False in select_related:
+            # We are not interested in the default value
+            select_related.remove(False)
+
+        if len(select_related) == 1:
+            res.query.select_related = select_related[0]
+        elif len(select_related) == 2:
+            if True in select_related:
+                # Prefer explicit select_related to generic select_related()
+                select_related.remove(True)
+
+            if len(select_related) > 0:
+                # If we have two explicit select_related calls, take any of them
+                res.query.select_related = select_related[0]
+            else:
+                res = res.select_related()
+
+        res._prefetch_related_lookups = list(
+            set(qs1._prefetch_related_lookups)
+            | set(qs2._prefetch_related_lookups))
+
+        return res
+
+    while tail:
+        head = _merge(head, tail[0])
+        tail = tail[1:]
+    return head
 
 
 _KWARG_RE = re.compile("(?:([-\w]+)=)?(.+)")
