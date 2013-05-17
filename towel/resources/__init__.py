3ab91e88c62c55939b50efb4d72a622f5632cd02
Matthias Kestenholz <mk@spinlock.ch>
2013-05-17 22:51:04 +0200

    Oops

--- towel/resources/__init__.py
+++ towel/resources/__init__.py
@@ -1,2 +1,2 @@
 from .base import (ModelResourceView, ListView, DetailView, FormView, AddView,
-    EditView, LiveFormView, PickerView, DeleteView)
+    EditView, LiveUpdateAfterEditMixin, LiveFormView, PickerView, DeleteView)
