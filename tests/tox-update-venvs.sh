b8a591bd3a64f14fd01aca2f2c91374a894662f7
Matthias Kestenholz <mk@spinlock.ch>
2013-12-21 15:01:35 +0100

    Django 1.7 does not support Python 2.6 anymore

--- tests/tox-update-venvs.sh
+++ tests/tox-update-venvs.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 (
-    cd .tox/py26-django17
-    bin/pip install -U --editable=git+git://github.com/django/django.git@master#egg=django-dev
-)
-(
     cd .tox/py27-django17
     bin/pip install -U --editable=git+git://github.com/django/django.git@master#egg=django-dev
 )
