e9956eea2b7cd44a76c57e9d5a87384baa8ccf36
Matthias Kestenholz <mk@feinheit.ch>
2017-11-25 15:57:41 +0100

    Also set MIDDLEWARE

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -46,7 +46,7 @@ TEMPLATES = [
         },
     },
 ]
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE_CLASSES = MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
